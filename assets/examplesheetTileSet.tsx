8878cb21f33818a83f6691f06ca06ce7d821e76b
SebastianJM <buy16@uib.no>
2023-04-05 03:42:39 +0200

    created working tilemap with seperate collision layer

---
+++ assets/examplesheetTileSet.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="examplesheetTileSet" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <image source="examplesheet.png" width="64" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="&quot;blocked&quot;" value=""/>
+  </properties>
+ </tile>
+</tileset>
